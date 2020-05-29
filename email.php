57061419160bde77a0e2bf390e095f20aa1e1509
SAYANTAN CHATTERJEE <64077426+SayantanChatterjeeDev@users.noreply.github.com>
2020-05-30 04:44:35 +0530

    Update email.php

--- email.php
+++ email.php
@@ -11,43 +11,27 @@ if(isset($_POST['reciever']) && isset($_POST['subject']) && isset($_POST['email_
     require_once "PHPMailer\\Exception.php";
     require_once "PHPMailer\\SMTP.php";
 
-    //PHPMailer Object
     $mail = new PHPMailer\PHPMailer\PHPMailer();
-    $mail->IsSMTP(); // enable SMTP
+    $mail->IsSMTP(); 
 
-    $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-    $mail->SMTPAuth = true; // authentication enabled
-    // $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-    $mail->Host = "smtp.hostinger.in";
-    $mail->Port = 587; // or 587
+    $mail->SMTPDebug = 1; 
+    $mail->SMTPAuth = true; 
+    $mail->Host = "";
+    $mail->Port = ***; 
     $mail->IsHTML(true);
-    $mail->Username = "mindalert@gyaansolution.com";
-    $mail->Password = "imsayantan2";
+    $mail->Username = "";
+    $mail->Password = "";
 
-    //From email address and name
-    $mail->From = "mindalert@gyaansolution.com";
-    $mail->FromName = "Sayantan Chatterjee";
+    $mail->From = "";
+    $mail->FromName = "";
 
-    //To address and name
-    // $mail->addAddress("recepient1@example.com", "Recepient Name");
     $mail->addAddress($reciever); //Recipient name is optional
 
-    // //Address to which recipient will reply
-    // $mail->addReplyTo("reply@yourdomain.com", "Reply");
 
-    // //CC and BCC
-    // $mail->addCC("cc@example.com");
-    // $mail->addBCC("bcc@example.com");
-
-    //Send HTML or Plain Text email
     $mail->isHTML(true);
 
     $mail->Subject = $subject;
     $mail->Body = $email_body ;
-    // $mail->AltBody = "This is the plain text version of the email content";
-
-
-
     if($mail->send()){
         echo json_encode("Mail sent successfully");
     }else{
